1840c2ffa6fd0703f227533f7eb6214ad6787a9a
John Duncan <john@johnduncanphoto.com>
2018-03-25 12:05:30 +0100

    initial commit

---
+++ word_count.rb
@@ -0,0 +1,11 @@
+class Phrase
+
+    def initialize (input_string) 
+        @input_string = input_string
+    end   
+    
+    def word_count()
+        @return_value = @input_string.length
+        return @return_value
+    end
+end 
