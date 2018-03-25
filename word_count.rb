a09ba88fcc9980cfba7b333c75476dacb12fbe18
John Duncan <john@johnduncanphoto.com>
2018-03-25 14:51:21 +0100

    all tests passing

--- word_count.rb
+++ word_count.rb
@@ -5,15 +5,32 @@ class Phrase
     end   
     
     def word_count()
-        @wordCount = {}
-        @stringArray = @input_string.split(" ")
-        @stringArray.each do | word |
-            if @wordCount.has_key?(word)
-                @wordCount[word] = @wordCount[word] + 1
+        word_count = {}
+        lower_case = @input_string.downcase
+        punctuation_removed = lower_case.gsub(/\p{^Alnum}/, ' ')
+        stringArray = punctuation_removed.split(" ")
+        stringArray.each do | word |
+            if word_count.has_key?(word)
+                word_count[word] = word_count[word] + 1
             else
-                @wordCount[word] = 1
+                word_count[word] = 1
             end
         end
-        return @wordCount
+    #   I realise I should replace all ' with a blank space first, then do a gsub for other characters rather than this
+        word_count.delete("t")
+        if word_count.include?("don")
+            word_count["don't"] = word_count.delete("don")
+            return word_count
+        elsif 
+            word_count.include?("can")
+            word_count["can't"] = word_count.delete("can")
+        else
+            
+        end
+         return word_count
     end
+
 end 
+
+
+# 
